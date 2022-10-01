ff7291049852212eba191dd4b254a7a2f8f55737
bilal karahan <bilalkarahan309@gmail.com>
2022-10-01 18:33:17 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class Main {
+    static void desen(int n,int y,int z){
+        System.out.print(" "+n);
+        if(n<=0) return;
+
+
+
+        desen(n-5,y++,z);
+        if(z>=n){
+            System.out.print(" "+n);
+            n+=5;
+        }
+
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in=new Scanner(System.in);
+        System.out.println("N sayısını giriniz : ");
+        int x= in.nextInt();
+        desen(x,1,x);
     }
 }
